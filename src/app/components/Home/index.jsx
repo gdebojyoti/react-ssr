a8f4728a39026efb1138db761cdb7f8e48a896ec
Debojyoti Ghosh <gdebojyoti.mail@gmail.com>
2019-10-01 16:16:32 +0530

    Redundant stuff removed.

--- src/app/components/Home/index.jsx
+++ src/app/components/Home/index.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
 const Home = () => {
+  const onClick = () => {
+    console.log('Clicked!')
+  }
+
   return (
     <div>
       <h1>Welcome to your SSR app!</h1>
+      <button onClick={onClick}>Click me!</button>
     </div>
   )
 }
